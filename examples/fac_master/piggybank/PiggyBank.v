c49eac93436cab2a78b1dc70cd999d0ee8c436c5
asmusbartram <ab@csep.dk>
2022-04-21 17:26:30 +0200

    Fixed a bug in piggybank and changed all proofs to use top level receive

--- examples/fac_master/piggybank/PiggyBank.v
+++ examples/fac_master/piggybank/PiggyBank.v
@@ -40,11 +40,14 @@ Section PiggyBank.
   
   Definition piggyBank (st : State) (msg : Msg) (ctx : ContractCallContext) : option (State * list ActionBody) :=
     match msg with
-    | Insert => 
-      match st with
-      | {| piggyState := Intact |} => Some ((insert ctx.(ctx_amount) st), [])
-      | _ => None 
-      end
+    | Insert =>
+      if 0 <? ctx.(ctx_amount)
+      then
+        match st with
+        | {| piggyState := Intact |} => Some ((insert ctx.(ctx_amount) st), [])
+        | _ => None 
+        end
+      else None
     | Smash =>
       match st with
       | {| balance := b; owner := o; piggyState := Intact |} => 
@@ -86,23 +89,28 @@ End PiggyBank.
 Section FunctionalProperties.
 
   Context {BaseTypes : ChainBase}.
-
-  Lemma piggybank_correct {prev_state next_state acts msg ctx} :
-  prev_state.(piggyState) = Intact -> piggyBank prev_state msg ctx = Some (next_state, acts) ->
-  match msg with
-  | Insert => prev_state.(balance) + ctx.(ctx_amount) = next_state.(balance)
-  | Smash => prev_state.(owner) = ctx.(ctx_from) -> next_state.(piggyState) = Smashed /\ 
-    next_state.(balance) = 0 /\ acts = [act_transfer prev_state.(owner) prev_state.(balance)]
-  end.
+  
+  Lemma piggybank_correct {prev_state next_state acts msg chain ctx} :
+    prev_state.(piggyState) = Intact -> piggyBank_receive chain ctx prev_state msg = Some (next_state, acts) ->
+    match msg with
+      | Some m => 
+        match m with
+        | Insert => prev_state.(balance) + ctx.(ctx_amount) = next_state.(balance)
+        | Smash => prev_state.(owner) = ctx.(ctx_from) -> next_state.(piggyState) = Smashed /\ 
+          next_state.(balance) = 0 /\ acts = [act_transfer prev_state.(owner) prev_state.(balance)]
+        end
+      | None => False
+    end.
   Proof.
-  intros H1 H2.
-  destruct msg.
-  - destruct prev_state. cbn in *. rewrite H1 in H2. unfold insert in *.
-    cbn in H2. inversion H2. cbn. reflexivity.
-  - intros H3. destruct prev_state. cbn in *. rewrite H1 in H2. rewrite H3 in H2. 
-    destruct (address_eqb_spec (ctx_from ctx) (ctx_from ctx)) in H2.
-    + inversion H2. cbn. rewrite H3. auto.
-    + discriminate.
+    intros H1 H2. unfold piggyBank_receive in H2. destruct msg. unfold piggyBank in H2.
+    destruct m.
+    - destruct prev_state. cbn in *. rewrite H1 in H2. unfold insert in *.
+      cbn in H2. destruct (0 <? ctx_amount ctx); try discriminate. inversion H2. cbn. reflexivity.
+    - intros H3. destruct prev_state. cbn in *. rewrite H1 in H2. rewrite H3 in H2. 
+      destruct (address_eqb_spec (ctx_from ctx) (ctx_from ctx)) in H2.
+      + inversion H2. cbn. rewrite H3. auto.
+      + discriminate.
+    - discriminate.
   Qed.
 
 End FunctionalProperties.
@@ -111,18 +119,30 @@ Section SafetyProperties.
 
   Context {BaseTypes : ChainBase}.
 
-  Lemma owner_never_changes {prev_state next_state msg ctx acts}:
-    piggyBank prev_state msg ctx = Some (next_state, acts) -> prev_state.(owner) = next_state.(owner).
+  Lemma owner_never_changes {prev_state next_state msg chain ctx acts}:
+  piggyBank_receive chain ctx prev_state msg = Some (next_state, acts) -> prev_state.(owner) = next_state.(owner).
   Proof.
-    intros H; destruct msg; cbn in *;destruct prev_state; cbn in *; destruct piggyState0; inversion H.
-    - auto.
-    - destruct (address_eqb_spec (ctx_from ctx) owner0) in H1; inversion H1; auto.
+    intros H. unfold piggyBank_receive in H. destruct msg. unfold piggyBank in H.
+    destruct m; cbn in *;destruct prev_state; cbn in *; destruct piggyState0; destruct (0 <? ctx_amount ctx); try easy.
+    inversion H. cbn in *. reflexivity.
+    - destruct (address_eqb_spec (ctx_from ctx) owner0) in H; inversion H; auto.
+    - destruct (address_eqb_spec (ctx_from ctx) owner0) in H; inversion H; auto.
+    - discriminate.  
   Qed.
 
-  Lemma cant_change_smashed {prev_state msg ctx}:
-    prev_state.(piggyState) = Smashed -> piggyBank prev_state msg ctx = None.
+  Lemma cant_change_smashed {prev_state msg chain ctx}:
+    prev_state.(piggyState) = Smashed -> piggyBank_receive chain ctx prev_state msg = None.
   Proof.
-    intros H. destruct msg, prev_state; cbn in *; rewrite H; auto.
+    intros H. unfold piggyBank_receive. destruct msg, prev_state; try auto. cbn in *. unfold piggyBank. rewrite H. destruct (0 <? ctx_amount ctx); destruct m; auto. 
   Qed.
 
+  Lemma if_intact_balance_only_increasing {prev_state next_state chain ctx new_acts}:
+    prev_state.(piggyState) = Intact ->
+    piggyBank_receive chain ctx prev_state (Some Insert) = Some (next_state, new_acts) ->
+    prev_state.(balance) <= next_state.(balance).
+  Proof.
+    intros H H1. destruct prev_state. cbn in *. rewrite H in H1. destruct (0 <? ctx_amount ctx) eqn:E; try easy.
+    inversion H1. cbn in *. apply Z.ltb_lt in E; omega.
+  Qed. 
+    
 End SafetyProperties.
