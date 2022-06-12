7a0420705bd7f1134ce3e92710875ff20e04a85e
weis611 <christian.weis.8270@gmail.com>
2022-06-12 14:11:28 +0200

    FA2Types coqdoc

--- examples/fac_master/tzwrap/fa2/multi_asset/FA2Types.v
+++ examples/fac_master/tzwrap/fa2/multi_asset/FA2Types.v
@@ -1,3 +1,8 @@
+(** * FA2 Types*)
+(** This file contains the implementation of the file
+https://github.com/bender-labs/wrap-tz-contracts/blob/1655949e61b05a1c25cc00dcb8c1da9d91799f31/ligo/fa2/multi_asset/types.mligo
+It Contains types required for the FA2 tokens contract.
+*)
 Require Import FA2Interface_Wrap.
 Require Import Containers.
 Require Import ZArith.
@@ -14,6 +19,7 @@ Section FA2Types.
 Context {BaseTypes: ChainBase}.
 Set Nonrecursive Elimination Schemes.
 
+(** ** Types for keeping track of state*)
 Definition Ledger := FMap (Address * token_id) N.
 
 Definition OperatorStorage := FMap (Address * (Address * token_id)) unit.
@@ -27,8 +33,10 @@ Record MultiTokenStorage :=   {
         mts_token_metadata : TokenMetaDataStorage
     }.
 
+(* begin hide *)
 Global Instance MultiTokenStorage_serializable : Serializable MultiTokenStorage :=
 Derive Serializable MultiTokenStorage_rect<Build_MultiTokenStorage>.
+(* end hide *)
 
 Record MultiAssetStorage :=  {
         fa2_admin : TokenAdminStorage ; 
@@ -36,12 +44,17 @@ Record MultiAssetStorage :=  {
         metadata : ContractMetadata
     }.
 
+(* begin hide *)
 Global Instance MultiAssetStorage_serializable : Serializable MultiAssetStorage :=
 Derive Serializable MultiAssetStorage_rect<Build_MultiAssetStorage>.
+(* end hide *)
 
+(** ** Return type for the FA2 token contract *)
 Definition Return: Type := (MultiAssetStorage * (list ActionBody)).
 
+(* begin hide *)
 MetaCoq Run (make_setters MultiTokenStorage).
 MetaCoq Run (make_setters MultiAssetStorage).
+(* end hide*)
 
 End FA2Types.
