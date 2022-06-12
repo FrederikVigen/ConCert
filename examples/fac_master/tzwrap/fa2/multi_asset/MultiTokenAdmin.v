6e75af754a98cf92f8742584ba58574d86bc94de
asmusbartram <ab@csep.dk>
2022-06-12 13:54:12 +0200

    MultiTokenAdmin

--- examples/fac_master/tzwrap/fa2/multi_asset/MultiTokenAdmin.v
+++ examples/fac_master/tzwrap/fa2/multi_asset/MultiTokenAdmin.v
@@ -1,3 +1,12 @@
+(** * Multi Token Admin *)
+(** This is an implementation of the following file.
+
+https://github.com/bender-labs/wrap-tz-contracts/blob/master/ligo/fa2/multi_asset/MultiTokenAdmin.mligo.
+
+Entrypoints for creating tokens and managing the contract
+
+*)
+
 Require Import ZArith.
 Require Import Blockchain.
 Require Import Containers.
@@ -11,10 +20,12 @@ Require Import Monads.
 Import ListNotations.
 
 Open Scope N_scope.
+(** * Implementation *)
 
 Section MultiTokenAdmin.
 Context {BaseTypes : ChainBase}.
 
+(** ** Create token *)
 Definition create_token (metadata : token_metadata) (storage : MultiTokenStorage) : option MultiTokenStorage :=
     let new_token_id := metadata.(metadata_token_id) in
     let existing_meta := FMap.find new_token_id storage.(mts_token_metadata) in
@@ -26,6 +37,7 @@ Definition create_token (metadata : token_metadata) (storage : MultiTokenStorage
         Some (storage<|mts_token_metadata := meta|><|token_total_supply := supply|>)
     end.
 
+(** ** Main entry point *)
 Definition multi_token_admin_main (ctx : ContractCallContext) (p : MultiTokenAdmin) (s: MultiAssetStorage) : option Return := 
     match p with
     | Token_admin p =>
