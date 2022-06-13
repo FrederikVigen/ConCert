3d217a6676287380cc15016251b42446126afa29
weis611 <christian.weis.8270@gmail.com>
2022-06-13 14:16:47 +0200

    Coqdoc operator lib and permissions descriptor

--- examples/fac_master/tzwrap/fa2/common/lib/FA2_Operator_Lib.v
+++ examples/fac_master/tzwrap/fa2/common/lib/FA2_Operator_Lib.v
@@ -1,3 +1,10 @@
+(** * FA2 Operator Lib*)
+(** This is an implementation of the following file
+
+https://github.com/bender-labs/wrap-tz-contracts/blob/1655949e61b05a1c25cc00dcb8c1da9d91799f31/ligo/fa2/common/lib/fa2_operator_lib.mligo
+
+It contains types for permissions
+*)
 Require Import FA2_Permissions_Descriptor.
 Require Import Blockchain.
 From ConCert.Examples.FA2 Require Import FA2Interface.
@@ -11,14 +18,17 @@ Section FA2_Operator_Lib.
 
 Context {BaseTypes : ChainBase}.
 
+(** ** Storage for operators *)
 Definition OperatorStorage := FMap (Address * (Address * token_id)) unit.
 
+(** ** Update Operators*)
 Definition update_operators (update : update_operator) (storage : OperatorStorage) : OperatorStorage :=
     match update with
     | add_operator op => FMap.update (op.(op_param_owner), (op.(op_param_operator), op.(op_param_token_id))) (Some tt) storage
     | remove_operator op => FMap.remove (op.(op_param_owner), (op.(op_param_operator), op.(op_param_token_id))) storage
     end.
 
+(** ** Validate Update Operators By Owner*)
 Definition validate_update_operators_by_owner (update : update_operator) (updater : Address) : option unit :=
     let op := match update with 
     | add_operator op => op
@@ -27,6 +37,7 @@ Definition validate_update_operators_by_owner (update : update_operator) (update
     in
     if address_eqb op.(op_param_owner) updater then Some tt else None.
 
+(** ** FA2 Update Operators*)
 Definition fa2_update_operators (ctx : ContractCallContext) (updates : list update_operator) (storage : OperatorStorage) : option OperatorStorage :=
     let updater := ctx.(ctx_from) in
     let process_update := fun (ops_opt : option OperatorStorage) (update : update_operator) =>
@@ -36,8 +47,10 @@ Definition fa2_update_operators (ctx : ContractCallContext) (updates : list upda
     in
     fold_left process_update updates (Some storage).
 
+(** ** Operator Validator*)
 Definition OperatorValidator := Address -> Address -> token_id -> OperatorStorage -> option unit.
 
+(** ** Make Operator Validator*)
 Definition make_operator_validator (tx_policy : OperatorTransferPolicy) : option OperatorValidator :=
     do can_tpl <- match tx_policy with 
     | NoTransfer => None
@@ -50,12 +63,14 @@ Definition make_operator_validator (tx_policy : OperatorTransferPolicy) : option
     else do _ <- FMap.find (owner, (operator, token_id)) ops_storage ;
     Some tt).
 
+(** ** Default Operator Validator*)
 Definition default_operator_validator : OperatorValidator :=
     fun (owner: Address) (operator : Address) (token_id : token_id) (ops_storage : OperatorStorage) =>
     if address_eqb owner operator then Some tt
     else do _ <- FMap.find (owner, (operator, token_id)) ops_storage;
     Some tt.
 
+(** ** Validate Operator*)
 Definition validate_operator (ctx: ContractCallContext) (tx_policy: OperatorTransferPolicy) (transfers: list transfer) (ops_storage : OperatorStorage)  : option unit :=
     do validator <- make_operator_validator tx_policy ;
     fold_left (fun (acc_opt : option unit) (tx: transfer) => 
